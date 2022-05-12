388f5efaefebbbffd5e1d183e3525ca1d22de8f4
Jeffrey Campbell <jeff.campbell@restlesslabs.com>
2022-05-12 07:26:13 +0500

    Initial Commit

---
+++ src/ios/CustomAlbum.m
@@ -0,0 +1,27 @@
+/********* CustomAlbum.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface CustomAlbum : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation CustomAlbum
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+@end
